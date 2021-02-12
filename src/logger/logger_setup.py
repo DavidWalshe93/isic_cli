3ab1a28d38e2877a9d3bc5bcb589f63056983d2f
DavidWalshe93 <david.walshe93@gmail.com>
2021-02-12 21:25:11 +0000

    Added missing module import and added file handler

--- src/logger/logger_setup.py
+++ src/logger/logger_setup.py
@@ -4,12 +4,13 @@ Date:       12 February 2021
 """
 
 import os
+import yaml
 import logging.config
 
 # Get the logger config in the same directory as this file.
 config_file_path = os.path.join(os.path.dirname(__file__), "logger_config.yml")
 
-with open(config_file_path, "r"):
+with open(config_file_path, "r") as fh:
     config = yaml.safe_load(fh.read())
 
     logging.config.dictConfig(config)
