c85805a7e3640bae2776361a4a07a6b769e72fbc
DavidWalshe93 <david.walshe93@gmail.com>
2021-02-12 21:11:55 +0000

    Added Click context config.py

---
+++ src/cli/config.py
@@ -0,0 +1,18 @@
+"""
+Author:     David Walshe
+Date:       12 February 2021
+"""
+
+# ===========================================================
+# Click Context Options
+# ===========================================================
+COMMAND_CONTEXT_SETTINGS = {
+    "context_settings": {
+        "help_options_names": ["-h", "--help"]
+    }
+}
+
+GROUP_CONTEXT_SETTINGS = {
+    **COMMAND_CONTEXT_SETTINGS,
+    "invoke_without_command": True
+}
